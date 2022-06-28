34593e37df35c4530909a07f31e74d1b8217f9a1
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-06-28 12:02:48 -0300

    FIX: (Challenge 15) Lint correction

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,17 +1,17 @@
 db.produtos.updateMany({}, {
-  $set: { avaliacao: NumberInt(0) }
+  $set: { avaliacao: NumberInt(0) },
 });
 
 db.produtos.updateMany({
   tags: { $all: ["bovino"] },
 }, {
-  $inc: { avaliacao: 5 }
+  $inc: { avaliacao: 5 },
 });
 
 db.produtos.updateMany({
   tags: { $all: ["ave"] },
 }, {
-  $inc: { avaliacao: 3 }
+  $inc: { avaliacao: 3 },
 });
 
 db.produtos.find({}, {
